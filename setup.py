99278fed4af5589735c030198cde565895cd8653
Josué M. Sehnem <josue@sehnem.com>
2022-09-19 12:00:21 -0400

    improve missing accounts log

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='target-netsuite',
-    version='1.0.16',
+    version='1.0.17',
     description='hotglue target for exporting data to NetSuite',
     author='hotglue',
     url='https://hotglue.xyz',

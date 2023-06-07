01de5607e5d6d385cd1b13579c433b1256e56a65
Hassan Syyid <hassansyyid@gmail.com>
2023-06-07 12:40:54 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='target-netsuite',
-    version='1.0.21',
+    version='1.0.23',
     description='hotglue target for exporting data to NetSuite',
     author='hotglue',
     url='https://hotglue.xyz',
